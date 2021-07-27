dc39fa7253993fec5bf635044723d51138577e90
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-07-27 11:46:31 +0200

    [add] Set the contact section

---
+++ sections/contact.php
@@ -0,0 +1,7 @@
+<div id="contact" class="contact">
+	<section class="half-part">
+		<h3 class="half-part__title"><?php the_field("contact_introduction_title"); ?></h3>
+		<p class="half-part__text"><?php the_field("contact_introduction_text"); ?></p>
+	</section>
+	<div><?php the_content(); ?></div>
+</div>
