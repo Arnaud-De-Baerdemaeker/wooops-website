58e9b141dd57e78fcaf9a304b5d17fb3a9faf933
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-08-02 16:25:23 +0200

    [refactor] Change the classes' names

--- sections/contact.php
+++ sections/contact.php
@@ -1,7 +1,7 @@
 <div id="contact_us" class="contact">
-	<section class="half-part">
-		<h3 class="half-part__title"><?php the_field("contact_introduction_title"); ?></h3>
-		<p class="half-part__text"><?php the_field("contact_introduction_text"); ?></p>
+	<section class="contact__introduction">
+		<h3 class="contact__title"><?php the_field("contact_introduction_title"); ?></h3>
+		<p class="contact__text"><?php the_field("contact_introduction_text"); ?></p>
 	</section>
 	<?php the_content(); ?>
 </div>
