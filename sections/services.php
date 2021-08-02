52b54ab6f57ccb2d400b4b070826bff605f68757
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-08-02 16:07:28 +0200

    [refactor] Move communication to a new file

--- sections/services.php
+++ sections/services.php
@@ -136,18 +136,4 @@
 			<h3 class="service6__title"><?php echo $s6_title; ?></h3>
 		</div>
 	<?php endif; ?>
-
-	<?php if ($communication): ?>
-		<div class="communication">
-			<h2 class="communication__title"><?php echo $communication["title"]; ?></h2>
-			<ul class="communication__list">
-				<li class="communication__item"><?php echo $communication["keywords"]["keyword_1"]; ?></li>
-				<li class="communication__item"><?php echo $communication["keywords"]["keyword_2"]; ?></li>
-				<li class="communication__item"><?php echo $communication["keywords"]["keyword_3"]; ?></li>
-				<li class="communication__item"><?php echo $communication["keywords"]["keyword_4"]; ?></li>
-				<li class="communication__item"><?php echo $communication["keywords"]["keyword_5"]; ?></li>
-				<li class="communication__item"><?php echo $communication["keywords"]["keyword_6"]; ?></li>
-			</ul>
-		</div>
-	<?php endif; ?>
 </div>
