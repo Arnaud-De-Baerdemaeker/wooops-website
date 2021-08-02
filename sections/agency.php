ee429585c12314b64e416bc62ad148fe8691197e
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-08-02 11:49:56 +0200

    [refactor] Add conditions around tags

--- sections/agency.php
+++ sections/agency.php
@@ -1,7 +1,14 @@
-<div id="our_agency" class="agency">
-	<section class="half-part">
-		<h3 class="half-part__title"><?php the_field("agency_title"); ?></h3>
-		<p class="half-part__text"><?php the_field("agency_text"); ?></p>
-	</section>
-	<div class="half-part"></div>
-</div>
+<?php $agency = get_field("agency");
+if ($agency): ?>
+	<div id="our_agency" class="agency">
+		<section class="half-part">
+			<?php if ($agency["agency_title"]): ?>
+				<h3 class="half-part__title"><?php echo $agency["agency_title"]; ?></h3>
+			<?php endif; ?>
+			<?php if ($agency["agency_text"]): ?>
+				<p class="half-part__text"><?php echo $agency["agency_text"]; ?></p>
+			<?php endif; ?>
+		</section>
+		<div class="half-part"></div>
+	</div>
+<?php endif; ?>
