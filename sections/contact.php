6a5e0c34c4bbb784d9f506f2d2d837283f33119f
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-08-04 16:41:43 +0200

    [refactor] Change the level of the title

--- sections/contact.php
+++ sections/contact.php
@@ -1,6 +1,6 @@
 <div id="contact_us" class="contact">
 	<section class="contact__introduction">
-		<h3 class="contact__title"><?php the_field("contact_introduction_title"); ?></h3>
+		<h2 class="contact__title"><?php the_field("contact_introduction_title"); ?></h2>
 		<p class="contact__text"><?php the_field("contact_introduction_text"); ?></p>
 	</section>
 	<?php the_content(); ?>
