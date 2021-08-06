dacc596a3b5ef6b1123fa1a3358c7e980b5b9285
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-08-06 11:46:26 +0200

    [refactor] Rework the footer's field and add date for copyright

--- footer.php
+++ footer.php
@@ -1,27 +1,25 @@
 <footer id="footer" class="footer">
+	<?php $footer = get_field("footer"); ?>
 	<div class="footer__informations">
 		<div class="footer__logo">
-			<?php $logo = get_field("footer_logo");
-			if ($logo): ?>
-				<img src="<?php echo esc_url($logo["url"]); ?>" alt="<?php echo esc_attr($logo["alt"]); ?>" class="footer__image" />
+			<?php if ($footer["logo"]): ?>
+				<img src="<?php echo esc_url($footer["logo"]["url"]); ?>" alt="<?php echo esc_attr($footer["logo"]["alt"]); ?>" class="footer__image" />
 			<?php endif; ?>
 		</div>
 		<div class="footer__contact-infos">
 			<section class="footer__address">
 				<h4 class="footer__title">Adresse</h4>
-				<?php $address = get_field("footer_address");
-				if ($address): ?>
-					<p class="footer__entry"><?php echo $address["street_and_number"]; ?></p>
-					<p class="footer__entry"><?php echo $address["postal_code_and_city"]; ?></p>
-					<p class="footer__entry"><?php echo $address["country"]; ?></p>
+				<?php if ($footer["address"]): ?>
+					<p class="footer__entry"><?php echo $footer["address"]["street_and_number"]; ?></p>
+					<p class="footer__entry"><?php echo $footer["address"]["postal_code_and_city"]; ?></p>
+					<p class="footer__entry"><?php echo $footer["address"]["country"]; ?></p>
 				<?php endif; ?>
 			</section>
 			<section class="footer__contact">
 				<h4 class="footer__title">Contact</h4>
-				<?php $contact = get_field("footer_contact");
-				if ($contact): ?>
-					<p class="footer__entry"><?php echo $contact["phone"]; ?></p>
-					<p class="footer__entry"><?php echo $contact["e-mail"]; ?></p>
+				<?php if ($footer["contact"]): ?>
+					<p class="footer__entry"><?php echo $footer["contact"]["phone"]; ?></p>
+					<p class="footer__entry"><?php echo $footer["contact"]["e-mail"]; ?></p>
 				<?php endif; ?>
 			</section>
 			<section class="footer__social">
@@ -29,5 +27,5 @@
 			</section>
 		</div>
 	</div>
-	<div class="footer__copyright"><?php echo date("Y"); ?></div>
+	<div class="footer__copyright">2019 - <?php echo date("Y"); ?> | <?php echo $footer["copyright"]; ?></div>
 </footer>
