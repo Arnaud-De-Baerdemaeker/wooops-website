67a84f40f50b29dc4d175b26a476091a20a40056
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-08-18 11:55:27 +0200

    [refactor] Add control over background image in WP

--- header.php
+++ header.php
@@ -6,15 +6,20 @@
 	</head>
 	
 	<body <?php body_class(); ?>>
-		<div id="hero" class="hero">
+		<?php $background = get_field("hero_background"); ?>
+		<div
+			id="hero"
+			<?php if ($background): ?>
+				style="background-image: url('<?php echo esc_url($background); ?>');"
+			<?php endif; ?>
+			class="hero"
+		>
 			<header class="header">
 				<?php $logo = get_field("hero_logo"); ?>
-
 				<h1 class="header__logo-container">
 					<?php if ($logo): ?>
 						<img src="<?php echo esc_url($logo["url"]); ?>" alt="<?php echo esc_attr($logo["alt"]); ?>" class="header__logo-image" />
 					<?php endif; ?>
 				</h1>
-
 				<?php wp_nav_menu(); ?>
 			</header>
