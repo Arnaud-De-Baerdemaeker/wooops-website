c8f7318c4f36a535d086a0c82b191daf7a21439a
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-09-01 11:57:39 +0200

    [refactor] Rework the structure of the section and ACF fields

--- sections/contact.php
+++ sections/contact.php
@@ -1,7 +1,16 @@
-<div id="contact_us" class="contact">
-	<section class="contact__introduction">
-		<h2 class="contact__title"><?php the_field("contact_introduction_title"); ?></h2>
-		<p class="contact__text"><?php the_field("contact_introduction_text"); ?></p>
-	</section>
-	<?php the_content(); ?>
-</div>
+<?php $contact = get_field("contact");
+if ($contact): ?>
+	<div id="contact_us" class="contact">
+		<h2 class="contact__title">
+			<?php echo $contact["title"]; ?>
+			<img src="<?php echo $contact["image"]["url"]; ?>" alt="<?php echo $contact["image"]["alt"]; ?>" class="contact__image" />
+		</h2>
+		
+		<div class="contact__content-container">
+			<div class="contact__introduction">
+				<p class="contact__text"><?php echo $contact["text"]; ?></p>
+			</div>
+			<?php the_content(); ?>
+		</div>
+	</div>
+<?php endif; ?>
