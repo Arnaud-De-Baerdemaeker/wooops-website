96c72bdb246e01e832d6d5e97a6aa053a79ce3eb
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-09-16 15:14:47 +0200

    [fix] Correct the functions to display from WordPress

--- sections/hero.php
+++ sections/hero.php
@@ -1,34 +1,32 @@
-<?php $background = get_field("hero_background"); ?>
-<?php $logo = get_field("hero_logo"); ?>
-<?php $introduction = get_field("introduction"); ?>
+<?php $hero = get_field("hero"); ?>
 
 <div
 	id="hero"
-	<?php if ($background): ?>
-		style="background-image: url('<?php echo esc_url($background); ?>');"
+	<?php if ($hero["background"]): ?>
+		style="background-image: url('<?php echo $hero["background"]; ?>');"
 	<?php endif; ?>
 	class="hero"
 >
 	<section class="introduction">
-		<?php if ($introduction["text_part_1"]): ?>
+		<?php if ($hero["introduction"]["text_part_1"]): ?>
 			<p class="introduction__text">
-				<?php echo $introduction["text_part_1"]["normal_text"]; ?>
+				<?php echo $hero["introduction"]["text_part_1"]["normal_text"]; ?>
 				<br />
-				<span class="introduction__keyword"><?php echo $introduction["text_part_1"]["keyword"]; ?></span>
+				<span class="introduction__keyword"><?php echo $hero["introduction"]["text_part_1"]["keyword"]; ?></span>
 			</p>
 		<?php endif; ?>
 
-		<?php if ($logo): ?>
+		<?php if ($hero["logo"]): ?>
 			<h1 class="introduction__logo-container">
-				<img src="<?php echo esc_url($logo["url"]); ?>" alt="Wooops Agency" class="introduction__logo-image" />
+				<img src="<?php echo $hero["logo"]["url"]; ?>" alt="Wooops Agency" class="introduction__logo-image" />
 			</h1>
 		<?php endif; ?>
 
-		<?php if ($introduction["text_part_2"]): ?>
+		<?php if ($hero["introduction"]["text_part_2"]): ?>
 			<p class="introduction__text">
-				<?php echo $introduction["text_part_2"]["normal_text"]; ?>
+				<?php echo $hero["introduction"]["text_part_2"]["normal_text"]; ?>
 				<br />
-				<span class="introduction__keyword"><?php echo $introduction["text_part_2"]["keyword"]; ?></span>
+				<span class="introduction__keyword"><?php echo $hero["introduction"]["text_part_2"]["keyword"]; ?></span>
 			</p>
 		<?php endif; ?>
 	</section>
