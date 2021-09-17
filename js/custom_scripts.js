ec24ea19379ddc7aefe6f9479f42629941b701fd
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-09-17 17:04:59 +0200

    [refactor] Add ScrollReveal effects

---
+++ js/custom_scripts.js
@@ -0,0 +1,59 @@
+// MENU BAR
+ScrollReveal().reveal(".header", {
+	origin: "top",
+	distance: "100px",
+	duration: 500
+});
+
+ScrollReveal().reveal(".menu-item-104", {
+	origin: "top",
+	distance: "100px",
+	duration: 1500
+});
+
+ScrollReveal().reveal(".menu-item-105", {
+	origin: "top",
+	distance: "100px",
+	duration: 2000
+});
+
+ScrollReveal().reveal(".menu-item-279", {
+	origin: "top",
+	distance: "100px",
+	duration: 2500
+});
+
+ScrollReveal().reveal(".menu-item-106", {
+	origin: "top",
+	distance: "100px",
+	duration: 3000
+});
+
+// 
+
+// AGENCY SECTION
+// Header
+ScrollReveal().reveal(".agency__title", {
+	origin: "top",
+	distance: "300px",
+	duration: 1000,
+	delay: 500
+});
+
+ScrollReveal().reveal(".title__text", {
+	origin: "left",
+	distance: "1000px",
+	duration: 1500,
+	delay: 500
+});
+
+ScrollReveal().reveal(".title__image", {
+	origin: "right",
+	distance: "1000px",
+	rotate: {
+		z: 180,
+	},
+	duration: 3000,
+	delay: 500,
+	viewFactor: 0.1
+});
