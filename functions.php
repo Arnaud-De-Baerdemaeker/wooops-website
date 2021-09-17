e7267373aced93eacffd8d89207f859583ecfdfc
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-09-17 17:03:27 +0200

    [refactor] Rework the registration of elements

--- functions.php
+++ functions.php
@@ -1,27 +1,21 @@
 <?php
 
 // Add custom tags in <head>
-function add_tags() {
-	?>
+function add_tags() { ?>
 	<meta charset="<?php bloginfo("charset"); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="Shortcut Icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" />
+	<link rel="Shortcut Icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri().'/style.css'; ?>">
+	<script src="https://unpkg.com/scrollreveal"></script>
+	<script src="<?php echo get_template_directory_uri().'/js/custom_scripts.js'; ?>" type="module"></script>
+	<!-- <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script> -->
 	<title><?php the_title(); ?></title>
 	<?php
 }
 add_action("wp_head", "add_tags");
-
-// Link a custom stylesheet to the theme
-function add_stylesheet() {
-	wp_enqueue_style("style", get_stylesheet_directory_uri()."/style.css", null, null, "all");
-}
-add_action("wp_enqueue_scripts", "add_stylesheet");
-
-// Link a Google font to the theme
-function add_fonts() {
-	wp_enqueue_style("google-fonts", "https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&family=Crimson+Text:wght@400;600;700&display=swap", array(), null);
-}
-add_action("wp_enqueue_scripts", "add_fonts");
 
 // Register a primary menu to the theme
 function register_menu() {
