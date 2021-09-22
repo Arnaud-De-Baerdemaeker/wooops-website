685fe0e1e1066fe04a8cb05f548c0b7c80fddaf0
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-09-22 16:23:48 +0200

    [refactor] Add condition for mobile and desktop behaviors

--- js/custom_scripts.js
+++ js/custom_scripts.js
@@ -1,8 +1,5 @@
-const sr = ScrollReveal({
-	cleanup: true
-});
-
-ScrollReveal().clean(".header, .menu-item-104, .menu-item-105, .menu-item-279, .menu-item-106, #text_1, .introduction__logo-container, #text_2, .jarallax-img, .agency__title, .services__title, .work__title, .contact__title, .title__text, .title__image, .service1__title, .service2__title, .service3__title, .service4__title, .service5__title, .service6__title, .service7__title, .service1a, #service2__item1, #service3__text1, .service4a, .contact__introduction, .service1b, #service2__item2, .service4b, .service4d, .service1c, #service2__item3, #service3__text2, .service4c, .wpcf7, #service7__item1, #service7__item2, #service7__item3, #service7__item4, #service7__item5, #service7__item6, .footer, .footer__logo, .footer__address, .footer__contact, .footer__social, .footer__copyright");
+const sr = ScrollReveal();
+const mq_w1000_up = window.matchMedia("(min-width: 1000px)");
 
 // ---- ALL DISPLAYS ----
 // MENU BAR
@@ -21,6 +18,12 @@ sr.reveal(".menu-item-104, .menu-item-105, .menu-item-279, .menu-item-106", {
 });
 
 // HERO
+sr.reveal(".jarallax-img", {
+	opacity: 0,
+	duration: 2000,
+	delay: 5000
+});
+
 sr.reveal("#text_1", {
 	origin: "left",
 	distance: "500%",
@@ -55,206 +58,172 @@ sr.reveal(".footer__logo", {
 // ---- END OF ALL DISPLAYS ----
 
 
-// ---- DESKTOP ----
-// HERO
-sr.reveal(".jarallax-img", {
-	opacity: 0,
-	duration: 2000,
-	delay: 5000
-})
+if (mq_w1000_up.matches) { // ---- DESKTOP ----
+	// H2 TITLES
+	sr.reveal(".agency__title, .services__title, .work__title, .contact__title", {
+		origin: "top",
+		distance: "100%",
+		duration: 1000,
+		viewOffset: {
+			bottom: 300
+		}
+	});
 
-// H2 TITLES
-sr.reveal(".agency__title, .services__title, .work__title, .contact__title", {
-	origin: "top",
-	distance: "100%",
-	duration: 1000,
-	viewOffset: {
-		bottom: 300
-	},
-	mobile: false
-});
+	sr.reveal(".title__text", {
+		origin: "left",
+		distance: "100%",
+		duration: 1500,
+		delay: 500,
+		viewOffset: {
+			bottom: 300
+		}
+	});
 
-sr.reveal(".title__text", {
-	origin: "left",
-	distance: "100%",
-	duration: 1500,
-	delay: 500,
-	viewOffset: {
-		bottom: 300
-	},
-	mobile: false
-});
+	sr.reveal(".title__image", {
+		origin: "right",
+		distance: "500%",
+		rotate: {
+			z: 180,
+		},
+		duration: 3000,
+		delay: 500,
+		viewOffset: {
+			bottom: 350
+		}
+	});
 
-sr.reveal(".title__image", {
-	origin: "right",
-	distance: "500%",
-	rotate: {
-		z: 180,
-	},
-	duration: 3000,
-	delay: 500,
-	viewOffset: {
-		bottom: 350
-	},
-	mobile: false
-});
+	// H3 TITLES
+	sr.reveal(".service1__title, .service2__title, .service3__title, .service4__title, .service5__title, .service6__title, .service7__title", {
+		origin: "top",
+		distance: "100%",
+		duration: 2000,
+		viewOffset: {
+			bottom: 250
+		}
+	});
 
-// H3 TITLES
-sr.reveal(".service1__title, .service2__title, .service3__title, .service4__title, .service5__title, .service6__title, .service7__title", {
-	origin: "top",
-	distance: "100%",
-	duration: 2000,
-	viewOffset: {
-		bottom: 250
-	},
-	mobile: false
-});
+	// CONTENT BOXES
+	sr.reveal(".service1a, #service2__item1, #service3__text1, .service4a, .contact__introduction", {
+		origin: "left",
+		distance: "100%",
+		duration: 2000,
+		viewOffset: {
+			bottom: 250
+		}
+	});
 
-// CONTENT BOXES
-sr.reveal(".service1a, #service2__item1, #service3__text1, .service4a, .contact__introduction", {
-	origin: "left",
-	distance: "100%",
-	duration: 2000,
-	viewOffset: {
-		bottom: 250
-	},
-	mobile: false
-});
+	sr.reveal(".service1b, #service2__item2, .service4b, .service4d", {
+		origin: "top",
+		distance: "100%",
+		duration: 2000,
+		viewOffset: {
+			bottom: 250
+		}
+	});
 
-sr.reveal(".service1b, #service2__item2, .service4b, .service4d", {
-	origin: "top",
-	distance: "100%",
-	duration: 2000,
-	viewOffset: {
-		bottom: 250
-	},
-	mobile: false
-});
+	sr.reveal(".service1c, #service2__item3, #service3__text2, .service4c, .wpcf7", {
+		origin: "right",
+		distance: "100%",
+		duration: 2000,
+		viewOffset: {
+			bottom: 250
+		}
+	});
 
-sr.reveal(".service1c, #service2__item3, #service3__text2, .service4c, .wpcf7", {
-	origin: "right",
-	distance: "100%",
-	duration: 2000,
-	viewOffset: {
-		bottom: 250
-	},
-	mobile: false
-});
+	sr.reveal("#service7__item1, #service7__item2, #service7__item3, #service7__item4, #service7__item5, #service7__item6", {
+		origin: "top",
+		distance: "100%",
+		duration: 1000,
+		viewOffset: {
+			bottom: 250
+		},
+		interval: 250
+	});
 
-sr.reveal("#service7__item1, #service7__item2, #service7__item3, #service7__item4, #service7__item5, #service7__item6", {
-	origin: "top",
-	distance: "100%",
-	duration: 1000,
-	viewOffset: {
-		bottom: 250
-	},
-	interval: 250,
-	mobile: false
-});
+	// FOOTER
+	sr.reveal(".footer__address", {
+		origin: "left",
+		distance: "100%",
+		duration: 2000,
+		delay: 1500
+	});
 
-// FOOTER
-sr.reveal(".footer__address", {
-	origin: "left",
-	distance: "100%",
-	duration: 2000,
-	delay: 1500,
-	mobile: false
-});
+	sr.reveal(".footer__contact", {
+		origin: "top",
+		distance: "100%",
+		duration: 2000,
+		delay: 1500
+	});
 
-sr.reveal(".footer__contact", {
-	origin: "top",
-	distance: "100%",
-	duration: 2000,
-	delay: 1500,
-	mobile: false
-});
+	sr.reveal(".footer__social", {
+		origin: "right",
+		distance: "100%",
+		duration: 2000,
+		delay: 1500
+	});
 
-sr.reveal(".footer__social", {
-	origin: "right",
-	distance: "100%",
-	duration: 2000,
-	delay: 1500,
-	mobile: false
-});
+	sr.reveal(".footer__copyright", {
+		opacity: 0,
+		duration: 2000,
+		delay: 2500
+	});
+}
+else { // ---- MOBILE ----
+	// H2 TITLES
+	sr.reveal(".agency__title, .services__title, .work__title, .contact__title", {
+		origin: "top",
+		distance: "10%",
+		duration: 1000,
+		viewOffset: {
+			bottom: 75
+		}
+	});
 
-sr.reveal(".footer__copyright", {
-	opacity: 0,
-	duration: 2000,
-	delay: 2500,
-	mobile: false
-});
-// ---- END OF DESKTOP ----
+	// H3 TITLES
+	sr.reveal(".service1__title, .service2__title, .service3__title, .service4__title, .service5__title, .service6__title, .service7__title", {
+		origin: "top",
+		distance: "100%",
+		duration: 2000,
+		viewOffset: {
+			bottom: 75
+		}
+	});
 
+	sr.reveal(".title__text", {
+		origin: "left",
+		distance: "500%",
+		duration: 1500,
+		delay: 500,
+		viewOffset: {
+			bottom: 75
+		}
+	});
 
-// ---- MOBILE ----
-// H2 TITLES
-sr.reveal(".agency__title, .services__title, .work__title, .contact__title", {
-	origin: "top",
-	distance: "100%",
-	duration: 1000,
-	viewOffset: {
-		bottom: 75
-	},
-	mobile: true,
-	desktop: false
-});
+	sr.reveal(".title__image", {
+		origin: "right",
+		distance: "500%",
+		rotate: {
+			z: 180,
+		},
+		duration: 3000,
+		delay: 500,
+		viewOffset: {
+			bottom: 100
+		}
+	});
 
-// H3 TITLES
-sr.reveal(".service1__title, .service2__title, .service3__title, .service4__title, .service5__title, .service6__title, .service7__title", {
-	origin: "top",
-	distance: "100%",
-	duration: 2000,
-	viewOffset: {
-		bottom: 75
-	},
-	mobile: true,
-	desktop: false
-});
+	// CONTENT BOXES
+	sr.reveal(".service1a, #service2__item1, #service3__text1, .service4a, .contact__introduction, .service1b, #service2__item2, .service4b, .service4d, .service1c, #service2__item3, #service3__text2, .service4c, .wpcf7, #service7__item1, #service7__item2, #service7__item3, #service7__item4, #service7__item5, #service7__item6, .footer__address, .footer__contact, .footer__social", {
+		origin: "top",
+		distance: "100%",
+		duration: 2000,
+		viewOffset: {
+			bottom: 75
+		}
+	});
 
-sr.reveal(".title__text", {
-	origin: "left",
-	distance: "500%",
-	duration: 1500,
-	delay: 500,
-	viewOffset: {
-		bottom: 75
-	},
-	mobile: true,
-	desktop: false
-
-});
-
-sr.reveal(".title__image", {
-	origin: "right",
-	distance: "500%",
-	rotate: {
-		z: 180,
-	},
-	duration: 3000,
-	delay: 500,
-	viewOffset: {
-		bottom: 100
-	},
-	mobile: true,
-	desktop: false
-});
-
-// CONTENT BOXES
-sr.reveal(".service1a, #service2__item1, #service3__text1, .service4a, .contact__introduction, .service1b, #service2__item2, .service4b, .service4d, .service1c, #service2__item3, #service3__text2, .service4c, .wpcf7, #service7__item1, #service7__item2, #service7__item3, #service7__item4, #service7__item5, #service7__item6, .footer__address, .footer__contact, .footer__social", {
-	origin: "top",
-	distance: "100%",
-	duration: 2000,
-	viewOffset: {
-		bottom: 75
-	},
-	mobile: true,
-	desktop: false
-});
-
-sr.reveal(".footer__copyright", {
-	opacity: 0,
-	duration: 2000,
-	mobile: true,
-	desktop: false
-});
-// ---- END OF MOBILE ----
+	sr.reveal(".footer__copyright", {
+		opacity: 0,
+		duration: 2000
+	});
+}
