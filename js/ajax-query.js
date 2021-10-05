16364e537cb201b263b16fb734ea8b9094932ea2
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-10-05 17:02:35 +0200

    [refactor] Build the AJAX call to filter the projects

--- js/ajax-query.js
+++ js/ajax-query.js
@@ -1,21 +1,27 @@
-// AJAX QUERIES
-// (function($) {
-// 	$(document).ready(function() {
-// 		$(document).on("click", ".projects__all-tags", function(e) {
-// 			e.preventDefault();
-//             var category = $(this).data("category");
+// AJAX
+(function($) {
+	$(document).ready(function() {
+		$(".projects__all-tags").on("click touchstart", function(event) {
+			event.preventDefault();
 
-//             $.ajax({
-//                 url: wp_ajax.ajax_url,
-//                 data: {
-//                     action: "filter",
-//                     category: category
-//                 },
-//                 type: "post",
-//                 success: function(result) {
-//                     console.warn(result);
-//                 }
-//             });
-// 		});
-// 	});
-// })(jQuery);
+			$(".projects__all-tags").removeClass("active");
+			$(this).addClass("active");
+
+			$.ajax({
+				type: "POST",
+				url: wp_ajax.ajax_url,
+				data: {
+					post_type: "projects",
+					action: "filter_projects",
+					tag: $(this).attr("id"), // Get the tag slug from element clicked
+				},
+				success: function(result) {
+					$(".projects__cards-container").html(result);
+				},
+				error: function(error) {
+					console.error("Something went wrong", error);
+				},
+			});
+		});
+	});
+})(jQuery);
