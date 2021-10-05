fdddeb3163575c5382a0f1efae4d2c749eb16e6a
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-10-05 17:05:58 +0200

    [refactor] Rework the file
    - Simplified the registration of the projects by using CPT
    - Built query to display all projects
    - Moved the list of projects in a template to avoid long code

--- sections/projects.php
+++ sections/projects.php
@@ -1,68 +1,50 @@
 <?php
-	$projects_header = get_field("projects_header");
-	$projects = get_field("projects");
+	$headers = get_field("projects");
 	$tags = get_the_tags();
 ?>
 
-<?php if ($projects_header["title"]): ?>
+<?php if ($headers["title"]): ?>
 	<div id="projects" class="projects">
 		<h2 class="projects__title">
-			<span class="title__text"><?php echo $projects_header["title"]; ?></span>
-			<?php if ($projects_header["image"]): ?>
-				<span class="title__image-container"><img src="<?php echo $projects_header["image"]["url"]; ?>" alt="<?php echo $projects_header["image"]["alt"]; ?>" class="title__image" /></span>
+			<span class="title__text"><?php echo $headers["title"]; ?></span>
+			<?php if ($headers["image"]): ?>
+				<span class="title__image-container"><img src="<?php echo $headers["image"]["url"]; ?>" alt="<?php echo $headers["image"]["alt"]; ?>" class="title__image" /></span>
 			<?php endif; ?>
 		</h2>
 
+		<!-- FILTER PROJECTS -->
 		<?php if ($tags): ?>
 			<div class="projects__all-tags-container">
 				<ul class="projects__all-tags-list">
+					<li>
+						<button id="all" class="projects__all-tags active">Tous les projets</button>
+					</li>
 					<?php foreach ($tags as $tag): ?>
 						<li>
-							<button class="projects__all-tags"><?php echo $tag->name; ?></button>
+							<button id="<?php echo $tag->slug; ?>" class="projects__all-tags"><?php echo $tag->name; ?></button>
 						</li>
 					<?php endforeach; ?>
 				</ul>
 			</div>
 		<?php endif; ?>
+		<!-- /FILTER PROJECTS -->
 
-		<?php if ($projects): ?>
+		<?php $get_projects = new WP_Query([
+			"post_type" => "projects",
+			"posts_per_page" => -1,
+			"orderby" => "menu_order",
+			"order" => "desc",
+		]);
+		
+		// LIST OF PROJECTS
+		if ($get_projects): ?>
 			<div class="projects__cards-container">
-				<?php foreach ($projects as $project):
-					if ($project): ?>
-						<div class="projects__cards">
-							<?php if ($project["client_name"]): ?>
-								<p class="projects__client-name"><?php echo $project["client_name"]; ?></p>
-							<?php endif; ?>
-							<div class="projects__thumbnails">
-								<?php if ($project["desktop_image"]): ?>
-									<img src="<?php echo $project["desktop_image"]["url"]; ?>" alt="<?php echo $project["desktop_image"]["alt"]; ?>" class="thumbnail__desktop">
-								<?php endif; ?>
-								<?php if ($project["mobile_image"]): ?>
-									<img src="<?php echo $project["mobile_image"]["url"]; ?>" alt="<?php echo $project["mobile_image"]["alt"]; ?>" class="thumbnail__mobile">
-								<?php endif; ?>
-							</div>
-							<div class="projects__informations">
-								<?php if ($project["presentation"]): ?>
-									<p class="projects__presentation"><?php echo $project["presentation"]; ?></p>
-								<?php endif;
-								// if ($project["specifications"]): ?>
-									<!-- <p class="projects__specifications"><?php //echo $project["specifications"]; ?></p> -->
-								<?php //endif;
-								if ($project["tags"]): ?>
-									<ul class="projects__post-tags-container">
-										<?php foreach ($project["tags"] as $tag): ?>
-											<li class="projects__post-tags"><?php echo $tag->name; ?></li>
-										<?php endforeach; ?>
-									</ul>
-								<?php endif;
-								if ($project["link"]): ?>
-									<a href="<?php echo $project["link"]; ?>" class="projects__link" target="_blank">Visiter</a>
-								<?php endif; ?>
-							</div>
-						</div>
-					<?php endif;
-				endforeach; ?>
+				<?php while ($get_projects->have_posts()): $get_projects->the_post();
+					get_template_part("sections/content-projets");
+				endwhile;
+				wp_reset_postdata(); ?>
 			</div>
 		<?php endif; ?>
+		<!-- /LIST OF PROJECTS -->
 	</div>
 <?php endif; ?>
