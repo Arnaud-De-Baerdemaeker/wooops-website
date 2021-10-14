23b3474a41fec6e9b366830bf28d0087c5a11ed2
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-10-14 16:24:24 +0200

    [feat] Build the conditions to control the messages

--- js/ajax_scripts.js
+++ js/ajax_scripts.js
@@ -1,8 +1,10 @@
 // AJAX
 (function($) {
 	$(document).ready(function() {
+		let page = 3;
+
 		if ($("#more").length > 0) {
-			$(".projects__cards-container").after("<button class='projects__load-more'>" + $("#more").attr("value") + "</button>")
+			$(".projects__cards-container").after("<button class='projects__more'>" + $("#more").attr("value") + "</button>")
 			$("#more").remove();
 		}
 		else if ($("#end").length > 0) {
@@ -30,6 +32,8 @@
 					tag: $(this).attr("id"), // Get the tag slug from element clicked
 				},
 				success: function(result) {
+					page = 3;
+
 					if ($(".projects__cards-container").length > 0) { // Check if the container exists
 						if ($(".projects__cards-container").html().length > 0) { // Check if the container has content
 							$(".projects__cards-container").empty().append(result); // If yes, then replace its content with the new one
@@ -43,42 +47,54 @@
 					}
 
 					if ($("#more").length > 0) {
-						$(".projects__cards-container").after("<button class='projects__load-more'>" + $("#more").attr("value") + "</button>")
-						$("#more").remove();
+						$(".projects__cards-container").after("<button class='projects__more'>" + $("#more").attr("value") + "</button>")
+						$("#more").hide();
 
-						if ($(".projects__load-more").length > 1) { 
-							$(".projects__load-more:not(:first)").remove();
+						if ($(".projects__more").length > 1) {
+							$(".projects__more:not(:last)").remove();
+							$(".projects__more").show();
 						}
 
 						if ($(".projects__end").length > 0) {
-							$(".projects__end").remove();
+							$(".projects__end").hide();
 						}
-						else if ($(".projects__empty").length > 0) {
-							$(".projects__empty").remove();
+						
+						if ($(".projects__empty").length > 0) {
+							$(".projects__empty").hide();
 						}
 					}
 					else if ($("#end").length > 0) {
 						$(".projects__cards-container").after("<div class='projects__end'>" + $("#end").attr("value") + "</div");
 						$("#end").remove();
-						$(".projects__end:not(:last)").remove();
 
-						if ($(".projects__load-more").length > 0) {
-							$(".projects__load-more").remove();
+						if ($(".projects__end").length > 1) {
+							$(".projects__end:not(:last)").remove();
+							$(".projects__end").show();
 						}
-						else if ($(".projects__empty").length > 0) {
-							$(".projects__empty").remove();
+
+						if ($(".projects__more").length > 0) {
+							$(".projects__more").hide();
+						}
+						
+						if ($(".projects__empty").length > 0) {
+							$(".projects__empty").hide();
 						}
 					}
 					else if ($("#empty").length > 0) {
 						$(".projects__cards-container").after("<div class='projects__empty'>" + $("#empty").attr("value") + "</div>");
-						$("#empty").remove();
-						$(".projects__empty:not(:last)").remove();
+						$("#empty").hide();
 
-						if ($(".projects__load-more").length > 0) {
-							$(".projects__load-more").remove();
+						if ($(".projects__empty").length > 1) {
+							$(".projects__empty:not(:last)").remove();
+							$(".projects__empty").show();
 						}
-						else if ($(".projects__end").length > 0) {
-							$(".projects__end").remove();
+
+						if ($(".projects__more").length > 0) {
+							$(".projects__more").hide();
+						}
+						
+						if ($(".projects__end").length > 0) {
+							$(".projects__end").hide();
 						}
 					}
 				},
@@ -89,8 +105,7 @@
 		});
 
 		// Load more posts after click
-		let page = 3;
-		$(".projects__load-more").on("click touchstart", function(event) {
+		$(".projects__more").on("click touchstart", function(event) {
 			event.preventDefault();
 
 			$.ajax({
@@ -99,45 +114,29 @@
 				data: {
 					post_type: "projects",
 					action: "load_more_projects",
-					tag: $(this).attr("id"), // Get the tag slug from element clicked
 					page: page
 				},
 				success: function(result) {
 					$(".projects__cards-container").append(result);
 					page++;
 
-					if ($("#more").length > 0) {
-						$(".projects__cards-container").after("<button class='projects__load-more'>" + $("#more").attr("value") + "</button>")
-						$("#more").remove();
-						$(".projects__load-more:not(:last)").remove();
-
-						if ($(".projects__end").length > 0) {
-							$(".projects__end").remove();
-						}
-						else if ($(".projects__empty").length > 0) {
-							$(".projects__empty").remove();
-						}
-					}
-					else if ($("#end").length > 0) {
+					if ($("#end").length > 0) {
 						$(".projects__cards-container").after("<div class='projects__end'>" + $("#end").attr("value") + "</div");
+						$(".projects__more").hide();
 						$("#end").remove();
 
-						if ($(".projects__load-more").length > 0) {
-							$(".projects__load-more").remove();
-						}
-						else if ($(".projects__empty").length > 0) {
-							$(".projects__empty").remove();
+						if ($(".projects__end").length > 1) {
+							$(".projects__end:not(:last)").remove();
+							$(".projects__end").show();
 						}
 					}
-					else if ($("#empty").length > 0) {
-						$(".projects__cards-container").after("<div class='projects__empty'>" + $("#empty").attr("value") + "</div>");
-						$("#empty").remove();
+					else if ($("#more").length > 0) {
+						$(".projects__cards-container").after("<button class='projects__more'>" + $("#more").attr("value") + "</button>")
+						$("#more").remove();
 
-						if ($(".projects__load-more").length > 0) {
-							$(".projects__load-more").remove();
-						}
-						else if ($(".projects__end").length > 0) {
-							$(".projects__end").remove();
+						if ($(".projects__more").length > 1) {
+							$(".projects__more:not(:last)").remove();
+							$(".projects__more").show();
 						}
 					}
 				},
