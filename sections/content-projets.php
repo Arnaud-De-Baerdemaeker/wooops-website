b2694b1411c67d1fb1f61d995613d3c286ab88d6
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-10-19 15:39:46 +0200

    [refactor] Rework the project's card structure
    - Used the link to englobe the card to remove the visit button
    - Made some fields required in the dashboard
    - Used a ternary condition for the tags

--- sections/content-projets.php
+++ sections/content-projets.php
@@ -4,40 +4,33 @@
 	$title = get_the_title();
 	$desktop_image = get_field("desktop_image");
 	$mobile_image = get_field("mobile_image");
-	$presentation = get_field("presentation");
-	$specifications = get_field("specifications");
+	//$presentation = get_field("presentation");
+	//$specifications = get_field("specifications");
 	$tags = get_field("tags");
-	$link = get_field("link");
+	$link = get_field("link") ? get_field("link") : "#";
 ?>
 
-<div class="projects__cards">
-	<?php if (!empty($title)): ?>
+<article class="projects__cards">
+	<a href="<?php echo $link; ?>" class="projects__link" target="_blank">
 		<p class="projects__client-name"><?php echo $title; ?></p>
-	<?php endif; ?>
-	<div class="projects__thumbnails">
-		<?php if (!empty($desktop_image["url"])): ?>
+		<div class="projects__thumbnails">
 			<img src="<?php echo $desktop_image["url"]; ?>" alt="<?php echo $desktop_image["alt"]; ?>" class="thumbnail__desktop">
-		<?php endif;
-		if (!empty($mobile_image["url"])): ?>
 			<img src="<?php echo $mobile_image["url"]; ?>" alt="<?php echo $mobile_image["alt"]; ?>" class="thumbnail__mobile">
-		<?php endif; ?>
-	</div>
-	<div class="projects__informations">
-		<?php // if (!empty($presentation): ?>
+		</div>
+		<div class="projects__informations">
 			<!-- <p class="projects__presentation"><?php //echo $presentation; ?></p> -->
-		<?php //endif;
-		// if (!empty($specifications)): ?>
 			<!-- <p class="projects__specifications"><?php //echo $specifications; ?></p> -->
-		<?php //endif;
-		if (!empty($tags)): ?>
-			<ul class="projects__post-tags-container">
-				<?php foreach ($tags as $tag): ?>
-					<li class="projects__post-tags"><?php echo $tag->name; ?></li>
-				<?php endforeach; ?>
-			</ul>
-		<?php endif;
-		if (!empty($link)): ?>
-			<a href="<?php echo $link; ?>" class="projects__link" target="_blank">Visiter</a>
-		<?php endif; ?>
-	</div>
-</div>
+			<?php
+			if (!empty($tags)):
+			?>
+				<ul class="projects__post-tags-container">
+					<?php foreach ($tags as $tag): ?>
+						<li class="projects__post-tags"><?php echo $tag->name; ?></li>
+					<?php endforeach; ?>
+				</ul>
+			<?php
+			endif;
+			?>
+		</div>
+	</a>
+</article>
