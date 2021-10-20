a63a04f9bf59893d1367377bbfe10764132a751e
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-10-20 16:52:20 +0200

    [feat] Custom scripts to control the main title's position on scroll

---
+++ js/jquery_scripts.js
@@ -0,0 +1,58 @@
+(function($) {
+	$(document).ready(function() {
+		// CONTROL KEYWORDS' DROPDOWN MENU ON CLICK
+		let screen_width = $(window).width();
+
+		if (screen_width < 1000) {
+			$(".projects__all-tags-list").hide();
+
+			$(".projects__select-tag").on("click", function() {
+				$(".projects__all-tags-list").toggle(300);
+			});
+
+			$(".projects__all-tags").on("click", function() {
+				$(".projects__all-tags-list").toggle(300);
+			});
+		}
+		else {
+			$(".projects__select-tag").hide();
+		}
+
+
+		// PUT THE LOGO IN THE HEADER WHEN SCROLLING
+		let screen_height = window.innerHeight;
+		let screen_scroll = window.scrollY;
+
+		$(".menu__wooops").hide();
+
+		if (screen_scroll >= screen_height / 2) {
+			$(".menu__wooops>a").text("Wooops Agency").replaceWith($(".introduction__logo-container"));
+			$(".introduction__logo-container").removeClass().addClass("introduction__logo-container--smaller");
+			$(".introduction__logo-image").removeClass().addClass("introduction__logo-image--smaller");
+			$(".menu__wooops").show();
+		}
+		else {
+			$(".menu__wooops").hide();
+			$(".introduction__logo-container--smaller").removeClass().addClass("introduction__logo-container");
+			$(".introduction__logo-image--smaller").removeClass().addClass("introduction__logo-image");
+			$(".introduction__logo-container").insertAfter("#text_1");
+		}
+
+		$(window).on("scroll", function() {
+			let screen_scroll = window.scrollY;
+
+			if (screen_scroll >= screen_height / 2) {
+				$(".menu__wooops>a").text("Wooops Agency").replaceWith($(".introduction__logo-container"));
+				$(".introduction__logo-container").removeClass().addClass("introduction__logo-container--smaller");
+				$(".introduction__logo-image").removeClass().addClass("introduction__logo-image--smaller");
+				$(".menu__wooops").show();
+			}
+			else {
+				$(".menu__wooops").hide();
+				$(".introduction__logo-container--smaller").removeClass().addClass("introduction__logo-container");
+				$(".introduction__logo-image--smaller").removeClass().addClass("introduction__logo-image");
+				$(".introduction__logo-container").insertAfter("#text_1");
+			}
+		});
+	});
+})(jQuery);
