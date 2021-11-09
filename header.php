3727b96856759a076e2e661b82bef6060b09e5be
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-11-09 15:59:16 +0100

    [feat] Add a close and shortcut icons

--- header.php
+++ header.php
@@ -9,12 +9,20 @@
 	
 	<body <?php body_class(); ?>>
 		<header class="header">
-			<button class="mobile-menu-toggle">
-				<img src="<?php echo get_template_directory_uri()."/icons/menu_icon.svg"; ?>" alt="Menu hamburger"/>
+			<button class="header__mobile-menu">
+				<img src="<?php echo get_template_directory_uri()."/icons/menu.svg"; ?>" alt="Menu hamburger" class="header__icon" />
+			</button>
+			<button class="header__close-menu">
+				<img src="<?php echo get_template_directory_uri()."/icons/close.svg"; ?>" alt="Fermer" class="header__icon" />
 			</button>
 			<?php
 			wp_nav_menu([
-				"menu" => "Menu principal",
+				"menu" => "Menu principal"
 			]);
 			?>
+			<div class="header__contact-shortcut">
+				<a href="<?php the_field("contact_shortcut") ?>">
+					<img src="<?php echo get_template_directory_uri()."/icons/email.svg"; ?>" alt="Contact" class="header__icon" />
+				</a>
+			</div>
 		</header>
