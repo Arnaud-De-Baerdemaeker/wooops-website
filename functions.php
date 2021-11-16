e57f0e1d0f4098bfac3d063d85ce74b570d1e7f1
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-11-16 10:49:08 +0100

    [feat] Add code to allow SVG files

--- functions.php
+++ functions.php
@@ -138,3 +138,32 @@ function register_jquery_scripts() {
 	wp_enqueue_script("jquery_scripts", get_template_directory_uri()."/js/jquery_scripts.js", array("jquery"), null, true);
 }
 add_action("wp_enqueue_scripts", "register_jquery_scripts");
+
+
+// ALLOW SVG
+add_filter("wp_check_filetype_and_ext", function($data, $file, $filename, $mimes) {
+	global $wp_version;
+
+	if ($wp_version !== "4.7.1") {
+		return $data;
+	}
+
+	$filetype = wp_check_filetype($filename, $mimes);
+
+	return [
+		"ext" => $filetype["ext"],
+		"type" => $filetype["type"],
+		"proper_filename" => $data["proper_filename"]
+	];
+}, 10, 4 );
+
+function cc_mime_types($mimes){
+	$mimes["svg"] = "image/svg+xml";
+	return $mimes;
+}
+add_filter("upload_mimes", "cc_mime_types");
+
+function fix_svg() {
+	echo "";
+}
+add_action("admin_head", "fix_svg");
