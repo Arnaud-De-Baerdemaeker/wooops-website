e0703044fd36ac0646b7ab74cbd5203a276f8922
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-07 12:28:23 +0100

    [refactor] Rework the code following a change in ACF

--- sections/hero.php
+++ sections/hero.php
@@ -1,36 +1,44 @@
-<?php $hero = get_field("hero"); ?>
+<?php
+$hero = get_field("hero");
+?>
 
 <div
 	id="hero"
-	<?php if ($hero["background"]): ?>
+	<?php
+	if ($hero["background"]):
+	?>
 		style="background-image: url('<?php echo $hero["background"]; ?>');"
-	<?php endif; ?>
+	<?php
+	endif;
+	?>
 	class="hero"
 >
-	<?php echo $hero["parallax"]; ?>
+	<?php
+	echo $hero["parallax"];
+	?>
 	<section class="introduction <?php if ($hero["gradient_effect"]) {echo "gradient";} ?>">
-		<?php if ($hero["introduction"]["text_part_1"]): ?>
-			<p id="text_1" class="introduction__text">
-				<?php echo $hero["introduction"]["text_part_1"]["normal_text"]; ?>
-				<br />
-				<span class="introduction__keyword"><?php echo $hero["introduction"]["text_part_1"]["keyword"]; ?></span>
-			</p>
-		<?php endif; ?>
+		<?php
+		if ($hero["introduction"]["text_part_1"]):
+			echo $hero["introduction"]["text_part_1"];
+		endif;
+		?>
 
-		<?php if ($hero["logo"]): ?>
+		<?php
+		if ($hero["logo"]):
+		?>
 			<a href="#top" class="introduction__logo-link">
 				<h1 class="introduction__logo-container">
 					<img src="<?php echo $hero["logo"]["url"]; ?>" alt="Wooops Agency" class="introduction__logo-image" />
 				</h1>
 			</a>
-		<?php endif; ?>
+		<?php
+		endif;
+		?>
 
-		<?php if ($hero["introduction"]["text_part_2"]): ?>
-			<p id="text_2" class="introduction__text">
-				<?php echo $hero["introduction"]["text_part_2"]["normal_text"]; ?>
-				<br />
-				<span class="introduction__keyword"><?php echo $hero["introduction"]["text_part_2"]["keyword"]; ?></span>
-			</p>
-		<?php endif; ?>
+		<?php
+		if ($hero["introduction"]["text_part_2"]):
+			echo $hero["introduction"]["text_part_2"];
+		endif;
+		?>
 	</section>
 </div>
